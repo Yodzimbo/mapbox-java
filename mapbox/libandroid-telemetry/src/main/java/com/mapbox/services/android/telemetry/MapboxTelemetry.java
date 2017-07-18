bf5a4645a03886f7cd941ceafff8cf0da0d28e6c
Guardiola31337 <pablo.guardiola@mapbox.com>
2017-07-18 21:00:58 +0200

    [WIP] fix location engine provider providing only those that are available

--- mapbox/libandroid-telemetry/src/main/java/com/mapbox/services/android/telemetry/MapboxTelemetry.java
+++ mapbox/libandroid-telemetry/src/main/java/com/mapbox/services/android/telemetry/MapboxTelemetry.java
@@ -24,9 +24,9 @@ import com.mapbox.services.android.telemetry.backoff.ExponentialBackoff;
 import com.mapbox.services.android.telemetry.connectivity.ConnectivityReceiver;
 import com.mapbox.services.android.telemetry.constants.TelemetryConstants;
 import com.mapbox.services.android.telemetry.http.TelemetryClient;
-import com.mapbox.services.android.telemetry.location.AndroidLocationEngine;
 import com.mapbox.services.android.telemetry.location.LocationEngine;
 import com.mapbox.services.android.telemetry.location.LocationEngineListener;
+import com.mapbox.services.android.telemetry.location.LocationEngineProvider;
 import com.mapbox.services.android.telemetry.permissions.PermissionsManager;
 import com.mapbox.services.android.telemetry.service.TelemetryService;
 import com.mapbox.services.android.telemetry.utils.TelemetryUtils;
@@ -363,7 +363,8 @@ public class MapboxTelemetry implements Callback, LocationEngineListener {
 
   private void registerLocationUpdates() {
     if (locationEngine == null) {
-      locationEngine = new AndroidLocationEngine(context);
+      LocationEngineProvider locationEngineProvider = new LocationEngineProvider(context);
+      locationEngine = locationEngineProvider.obtainAvailableLocationEngines().get(0);
     }
 
     locationEngine.addLocationEngineListener(this);
